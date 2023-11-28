1307bf5aa860dc065b7d28ade7a19edaa01a831e
Jonathan Liaw <j-liaw@hotmail.com>
2023-11-28 13:22:56 +0100

    Clean up at later point: Implement class

--- plotOA.py
+++ plotOA.py
@@ -1,3 +1,4 @@
+import sys
 import pickle
 import os, sys
 import matplotlib.pyplot as plt
@@ -6,7 +7,7 @@ import seaborn as sns; sns.set()
 from pdfSegregationBased_FS import segregateX_y
 from pdfSegregationBased_FS import compute_OA, plot_overlapAreas
 
-dsname = "lung"
+dsname = "geneExpressionCancerRNA"
 with open("ranking_allFeaturesOA.pkl", "rb") as handle:
     _dict = pickle.load(handle)
 
@@ -16,12 +17,11 @@ with open("datasets/processedDatasets.pkl", "rb") as handle:
 dataset = dataset_dict[dsname]
 X = dataset['X']
 y = dataset['y']
-
+print(X.shape)
 featureRanking = _dict[dsname]
-topFeature = featureRanking[12]
-lowFeature = featureRanking[-1]
+topFeature = featureRanking[20]
+lowFeature = featureRanking[-5]
 
-# fig, axs = plt.subplots(2, 1, sharex=True, sharey=True, figsize=(6.75, 7))
 y_segregatedGroup = segregateX_y(X, y)
 
 # Intra-feature normalization and computing the overlap area among classes
@@ -29,15 +29,19 @@ yLabels = list(y_segregatedGroup.keys())
 yLabels.sort()
 
 # Plot
-fig, ax = plt.subplots(1,2)
+fig, ax = plt.subplots(2, 1, figsize=(5.5,9.))
 topOA, topKernels, topLengths = compute_OA(yLabels, y_segregatedGroup, topFeature)
 plot_overlapAreas(
     topKernels, topLengths, topOA, "Feature values (normalized)", "PDF", ax[0]
 )
+ax[0].set_title("Example of a top feature")
 lowOA, lowKernels, lowLengths = compute_OA(yLabels, y_segregatedGroup, lowFeature)
 plot_overlapAreas(
     lowKernels, lowLengths, lowOA, "Feature values (normalized)", "PDF", ax[1]
 )
+ax[1].set_title("Example of a feature of low importance")
 
 plt.tight_layout()
 plt.show()
+
+sys.exit(0)
