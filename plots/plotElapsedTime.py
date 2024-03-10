49c1c4d250d20596b8a38fa8aca2505e41a41ccd
Jonathan Liaw <j-liaw@hotmail.com>
2024-03-10 21:45:09 +0100

    Update plotElapsedTime.py

--- plots/plotElapsedTime.py
+++ plots/plotElapsedTime.py
@@ -3,15 +3,16 @@ import os, sys
 import numpy as np
 import pandas as pd
 from pathlib import Path
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
 
-if len(sys.argv) < 5:
-    print("Possible usage: python3 plotElapsedTime.py <pyTime> <IRlf> <LHRlf> <savefile>")
+if len(sys.argv) < 4:
+    print("Possible usage: python3 plotElapsedTime.py <pyTime> <IRlf> <LHRlf>")
     sys.exit()
 else:
     pytime = Path(sys.argv[1])
     IR_time = Path(sys.argv[2])
     LHR_time = Path(sys.argv[3])
-    csvfile = sys.argv[4]
 
 with open(pytime, "rb") as handle:
     pytimes = pickle.load(handle)
@@ -19,13 +20,16 @@ with open(pytime, "rb") as handle:
 IRlf = pd.read_csv(IR_time, header=None, index_col=0)
 LHRlf = pd.read_csv(LHR_time, header=None, index_col=0)
 
-FSS = [
-    "RlfF", "MSurf", "I-RELIEF", "LH-RELIEF",
-    "MI",
-    "RFGini",
-    "FT",
-    "OAtotal", "OApw"
-]
+FSS = {
+    "RlfF": "RELIEF-F",
+    "MSurf": "MultiSURF",
+    "I-RELIEF": "I-RELIEF",
+    "LH-RELIEF": "LH-RELIEF",
+    "MI": "Mutual Information",
+    "RFGini": "Random Forest (Gini)",
+    "FT": "ANOVA (F-Statistic)",
+    "OAtotal": "PDE-Segregate"
+}
 
 datasets = [
     "cns", "lung", "leuk",
@@ -38,7 +42,7 @@ elapsedTimes_df = pd.DataFrame(
     index=datasets, columns=FSS
 )
 
-for fss in FSS:
+for fss in FSS.keys():
     for ds in datasets:
         if fss == "I-RELIEF":
             elapsedTimes_df.at[ds, fss] = IRlf.at[ds, 1]
@@ -55,10 +59,52 @@ averagedTimes_row = pd.DataFrame(
 )
 
 elapsedTimes_df = elapsedTimes_df._append(averagedTimes_row)
+print(elapsedTimes_df)
 
-elapsedTimes_df.to_csv(f"{csvfile}.csv")
+df_for_plotting = pd.DataFrame(
+    data=np.zeros((elapsedTimes_df.shape[0]*elapsedTimes_df.shape[1], 3)),
+    columns=["FSS", "Dataset", "Time"]
+)
+i = 0
+for fss in elapsedTimes_df.columns:
+    for ds in datasets:
+        df_for_plotting.at[i, "FSS"] = FSS[fss]
+        df_for_plotting.at[i, "Dataset"] = ds
+        df_for_plotting.at[i, "Time"] = elapsedTimes_df.at[ds, fss]
+        i += 1
+
+    df_for_plotting.at[i, "FSS"] = FSS[fss]
+    df_for_plotting.at[i, "Dataset"] = "Average"
+    df_for_plotting.at[i, "Time"] = elapsedTimes_df.at["Average", fss]
+    i += 1
+
+fig, ax = plt.subplots(1, 2, figsize=(14, 5))
+axBoxPlot = sns.boxplot(
+    data=df_for_plotting, x="Time", y="FSS",
+    width=0.7, fliersize=0., fill=False, ax=ax[0]
+)
+ax[0].set_xlim((0.0, 4500.0))
+ax[0].set_ylabel("")
+ax[0].set_title(
+    "Elapsed Times per Feature Selection Method", fontsize="xx-large",
+    x=0.0, y=1., horizontalalignment="left"
+)
+ax[0].tick_params(axis="both", labelsize="large")
+
+df_for_plotting_reduced = df_for_plotting[df_for_plotting["FSS"] != "I-RELIEF"]
+df_for_plotting_reduced = df_for_plotting_reduced[
+    df_for_plotting_reduced["FSS"] != "LH-RELIEF"
+]
+
+axBoxPlot2 = sns.boxplot(
+    data=df_for_plotting_reduced, x="Time", y="FSS",
+    width=0.7, fliersize=0., fill=False, ax=ax[1]
+)
+ax[1].set_xlim((0.0, 120.0))
+ax[1].set_ylabel("")
+ax[1].tick_params(axis="both", labelsize="large")
+
+plt.tight_layout()
+plt.show()
 
 sys.exit()
-
-
-    
